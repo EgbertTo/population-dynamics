3c4f0bd169f039eeaaeab3a4229ec4d22c10d382
jorenham <jhammudoglu@gmail.com>
2021-11-17 00:06:47 +0100

    Tidied up slider-related code duplication

--- population_dynamics.py
+++ population_dynamics.py
@@ -5,6 +5,16 @@ from matplotlib import widgets
 from scipy.integrate import odeint
 import seaborn as sns
 
+# parameter name -> (initial value, (min, max))
+PARAMS = {
+    'r': (1, (0, 6)),
+    'alpha': (1, (0, 2)),
+    'beta': (1, (0, 2)),
+    'K': (1_000, (100, 10_000)),
+    'P0': (0, (0, 100_000)),
+    't_max': (180, (1, 1_000)),
+}
+
 
 def dP_dt(
     t: int,
@@ -14,6 +24,10 @@ def dP_dt(
     beta: float,
     K: float,
 ) -> float:
+    """
+    d/dt P(t)
+    The differential equation of the logistic population curve.
+    """
     return r * ((K - P) / (K - (1 - beta) * P)) * P**alpha
 
 
@@ -25,19 +39,19 @@ def P_t(
     P0: float = 1_000,
     t_max: float = 180,
 ) -> tuple[np.ndarray, np.ndarray]:
+    """
+    P(t)
+    Numeric approximation of the solution of d/dt P(t).
+    """
     t = np.arange(0, t_max)
     return t, odeint(dP_dt, P0, t, args=(r, alpha, beta, K))[:, 0]
 
 
 # noinspection PyPep8Naming
 def main():
-    r0, alpha0, beta0 = 1.0, 1.0, 1.0
-    K0 = 1_000
-    t_min, t_max = 0, 180
-
     # Create the figure and the (initial) line
     fig, ax = plt.subplots(figsize=(16, 9))
-    line, = plt.plot(*P_t(r0, alpha0, beta0, K0, t_min, t_max), label='P(t)')
+    line, = plt.plot(*P_t(*(v for v, _ in PARAMS.values())), label='P(t)')
     ax.set_xlabel('$t$')
     ax.set_ylabel('$P(t)$')
     ax.margins(x=0)
@@ -46,78 +60,32 @@ def main():
     plt.subplots_adjust(left=0.075, top=0.95, bottom=0.1, right=9/16)
 
     # Construct the sliders to control the parameters
-    slider_r = widgets.Slider(
-        ax=plt.axes([0.65, 0.1, 0.0225, 0.63]),
-        label='r',
-        valmin=0.01,
-        valmax=6,
-        valinit=r0,
-        orientation='vertical',
-    )
-    slider_alpha = widgets.Slider(
-        ax=plt.axes([0.7, 0.1, 0.0225, 0.63]),
-        label='alpha',
-        valmin=0,
-        valmax=2,
-        valinit=alpha0,
-        orientation='vertical',
-    )
-    slider_beta = widgets.Slider(
-        ax=plt.axes([0.75, 0.1, 0.0225, 0.63]),
-        label='beta',
-        valmin=0,
-        valmax=2,
-        valinit=beta0,
-        orientation='vertical',
-    )
-    slider_K = widgets.Slider(
-        ax=plt.axes([0.8, 0.1, 0.0225, 0.63]),
-        label='K',
-        valmin=100,
-        valmax=10_000,
-        valinit=1_000,
-        orientation='vertical',
-    )
-    slider_P0 = widgets.Slider(
-        ax=plt.axes([0.85, 0.1, 0.0225, 0.63]),
-        label='P0',
-        valmin=0,
-        valmax=1_000_000,
-        valinit=1_000,
-        orientation='vertical',
-    )
-    slider_t = widgets.Slider(
-        ax=plt.axes([0.91, 0.1, 0.0225, 0.63]),
-        label='t',
-        valmin=0,
-        valmax=10_000,
-        valinit=1_000,
-        orientation='vertical',
-    )
+    sliders = {}
+    slider_position_x = 0.65
+    for name, (value0, (a, b)) in PARAMS.items():
+        sliders[name] = widgets.Slider(
+            ax=plt.axes([slider_position_x, 0.1, 0.0225, 0.63]),
+            label=name,
+            valmin=a,
+            valmax=b,
+            valinit=value0,
+            orientation='vertical',
+        )
+        slider_position_x += 0.05  # 5% horizontal padding between the sliders
 
     # Update the line on slider changes
     def update_line(_):
-        t, y = P_t(
-            slider_r.val,
-            slider_alpha.val,
-            slider_beta.val,
-            slider_K.val,
-            slider_P0.val,
-            slider_t.val,
-        )
+        param_vals = {_name: _slider.val for _name, _slider in sliders.items()}
+        t, y = P_t(**param_vals)
 
         line.set_xdata(t)
         ax.set_xlim(0, t.max())
 
         line.set_ydata(y)
-        ax.set_ylim(0, y.max())
+        ax.set_ylim(0, y.max() * 1.05)  # 5% margin
 
-    slider_r.on_changed(update_line)
-    slider_alpha.on_changed(update_line)
-    slider_beta.on_changed(update_line)
-    slider_K.on_changed(update_line)
-    slider_P0.on_changed(update_line)
-    slider_t.on_changed(update_line)
+    for slider in sliders.values():
+        slider.on_changed(update_line)
 
     # Display
     plt.show()
